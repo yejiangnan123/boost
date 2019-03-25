082d589bad71b1301c140597dc17b4a41d811fad
yejiangnan123 <41613597+yejiangnan123@users.noreply.github.com>
2019-03-25 10:06:42 +0800

    Update async_udp_echo_server.cpp

--- asio/echo/async_udp_echo_server.cpp
+++ asio/echo/async_udp_echo_server.cpp
@@ -1,3 +1,11 @@
+//boost::asio::io_context io_context;
+//udp::socket socket_;
+//udp::endpoint sender_endpoint_;
+//socket_(io_context, udp::endpoint(udp::v4(), port))
+//socket_.async_receive_from(
+//boost::asio::buffer(data_, max_length), sender_endpoint_,[this](boost::system::error_code ec, std::size_t bytes_recvd)
+//socket_.async_send_to(boost::asio::buffer(data_, length), sender_endpoint_,[this](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/)
+//io_context.run();
 #include <cstdlib>
 #include <iostream>
 #include <boost/asio.hpp>
